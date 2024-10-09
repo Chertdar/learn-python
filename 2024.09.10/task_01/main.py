48f5f3dfadf50162b6f7312410be9182870ec4f5
Chertdar <153106857+Chertdar@users.noreply.github.com>
2024-10-09 20:10:30 +0300

    created main.py
    erm? added some code and changed text in txt file

---
+++ 2024.09.10/task_01/main.py
@@ -0,0 +1,5 @@
+def split_text(s: str, separator: str) -> list[str]:
+    return s.split(sep=separator)
+if __name__ == '__main__':
+    test_str = "Вышел корень из тумана. Вынул ножик из кармана. Раз, два, всё."
+    print(split_text(test_str, '.'))
