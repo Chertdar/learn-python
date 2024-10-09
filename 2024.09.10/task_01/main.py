9806c4934d8b39933f02da97f83d16127491c0a9
Chertdar <153106857+Chertdar@users.noreply.github.com>
2024-10-09 21:08:12 +0300

    Update main.py
    i need to comlete program :P

--- 2024.09.10/task_01/main.py
+++ 2024.09.10/task_01/main.py
@@ -1,5 +1,10 @@
-def split_text(s: str, separator: str) -> list[str]:
-    return s.split(sep=separator)
+def split_text(txt: str, separas: list[str]) -> list[str]:
+    result: list[str] = ['']
+    waiting_for_serparator: bool = True
+
+    for symbol in txt:
+    return result
 if __name__ == '__main__':
     test_str = "Вышел корень из тумана. Вынул ножик из кармана. Раз, два, всё."
-    print(split_text(test_str, '.'))
+    separators: list[str] = [';', '.']
+    print(split_text(test_str, separators)
