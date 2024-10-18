45296df19211fa9d415593e07cf87286753c63ca
Chertdar <153106857+Chertdar@users.noreply.github.com>
2024-10-18 21:00:53 +0300

    ААААААААА, я забыла про git

---
+++ 2024.10.18/woof.py
@@ -0,0 +1,15 @@
+import requests
+
+def  get_list_of_available_countries() -> list[dict[str, str]]:
+    url = 'https://date.nager.at/api/v3/AvailableCountries'
+    res = requests.get(url).json()
+    return res
+def main() -> None:
+    countries = get_list_of_available_countries()
+    for country in countries:
+        if country['name'] == 'Russia':
+            print(f"Страна Russia, код [{country['countryCode']}]")
+            return
+    print('Страна Russia не найдена')
+if __name__ == '__main__':
+    main()
